c07c990e953eb31427a797b8b57fad95f6666f89
flavioramonnunes <f.ramon_paixao@hotmail.com>
2023-04-18 20:52:43 -0300

    Implementação de rotas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/ultracarSystem.css';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import PageNotFound from './pages/PageNotFound';
+import Home from './pages/Home';
+import ServiceNew from './pages/ServiceNew';
+import Services from './pages/Services';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={ Home } />
+            <Route path="/service/new" component={ ServiceNew } />
+            <Route path="/services" component={ Services } />
+            <Route path="*" component={ PageNotFound } />
+          </Switch>
+        </BrowserRouter>
+      </>
+    );
+  }
 }
 
 export default App;
